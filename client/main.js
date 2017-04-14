0ea44beae86945c1942685dfc08744f26ca1aa4d
Ivan Baena <ivan.baena04@gmail.com>
2017-04-14 11:06:29 -0400

    setup project

---
+++ client/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+import '/imports/startup/client'
+
+
+
+Meteor.startup(() => {
+  if (screen.width <= 1023) {
+
+  }
+});
---
+++ imports/ui/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+// This component - represents a single page
+export const Home = () =>
+
+<div>
+  <div id="home" className="desktop-sites">
+    HOME
+  </div>
+</div>
